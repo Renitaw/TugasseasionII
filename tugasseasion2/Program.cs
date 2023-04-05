e4ae4854e6e8d831306a187d4c66b9e00d5a8e2a
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-04-05 09:42:33 +0700

    membuat case k

--- tugasseasion2/Program.cs
+++ tugasseasion2/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace tugasseasion2
 {
@@ -15,7 +16,43 @@ namespace tugasseasion2
             Program pr = new Program();
             while (true)
             {
-
+                try
+                {
+                    Console.WriteLine("Koneksi Ke Database\n");
+                    Console.WriteLine("Masukkan User ID: ");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan password");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan database tujuan");
+                    string db = Console.ReadLine();
+                    Console.WriteLine("\nKetik K untuk Terhubung ke Database: ");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                    switch(chr)
+                    {
+                        case 'K':
+                            {
+                                SqlConnection conn = null;
+                                string strKoneksi = "Data source = RENITAWIDIASTUT\\RENITAWDST; " +
+                                    "initial catalog = {0}; " +
+                                    "user ID = {1} password = {2}";
+                                conn = new SqlConnection(string.Format(strKoneksi, db, user, pass));
+                                conn.Open();
+                                Console.Clear();
+                                while (true)
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine("\nMenu");
+                                        Console.WriteLine("1. Melihat Seluruh Data");
+                                        Console.WriteLine("2. Tambah Data");
+                                        Console.WriteLine("3. Keluar");
+                                        Console.Write("\nEnter your choice (1-3): ");
+                                        char ch = Convert.ToChar(Console.ReadLine());
+                                    }
+                                }
+                            }
+                    }
+                }
             }
         }
     }
